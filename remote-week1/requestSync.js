3c1de2abc322b82330a1bca4928ebbfb2e300784
Viktor879 <100680119+Viktor879@users.noreply.github.com>
2023-09-30 14:21:26 +0800

    Update requestSync.js
    Fix the bug of originally still sending async request, and using axios library this time to make the program more readable and easier to debug.

--- remote-week1/requestSync.js
+++ remote-week1/requestSync.js
@@ -1,19 +1,21 @@
-const url = "https://ec2-54-64-246-136.ap-northeast-1.compute.amazonaws.com/delay-clock";
-const request = require('request');
+const axios = require("axios");
 
-function requestSync(url) {
-  const startTime = new Date();
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error("Error:", error);
-      return;
-    }
-    const endTime = new Date();
-    console.log(`Sync Request Time: ${endTime - startTime}ms`);
-  });
+const url = "https://ec2-54-64-246-136.ap-northeast-1.compute.amazonaws.com/delay-clock";
+
+async function requestSync(url) {
+  const startTime = new Date().getTime(); // Record start time
+  await axios.get(url);
+  const endTime = new Date().getTime(); // Record end time after the request completes
+  return endTime - startTime; // Return the execution time
 }
 
-requestSync(url);
-requestSync(url);
-requestSync(url);
+async function runRequests() {
+  for (let i = 0; i < 3; i++) {
+    const executionTime = await requestSync(url);
+    console.log(`Execution time ${i + 1} (ms): ${executionTime}`);
+  }
+
+}
+
+runRequests();
 
